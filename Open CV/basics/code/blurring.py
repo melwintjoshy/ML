d1e534a76dacf4033d5e28c5a161e9065bf9a892
mj465 <melwin465@gmail.com>
2024-12-30 17:37:23 +0530

    intial commit

---
+++ Open CV/basics/code/blurring.py
@@ -0,0 +1,18 @@
+import cv2
+
+#The main use case of blurring is to remove the noise.
+
+image_path = r"C:\Users\melwi\Desktop\ML\project1\Open CV\basics\noise.png"
+
+img = cv2.imread(image_path)
+
+k_size = 7
+img_blur = cv2.blur(img, (k_size, k_size))
+img_gaussian_blur = cv2.GaussianBlur(img, (k_size, k_size), 5)
+img_median_blur = cv2.medianBlur(img, k_size)
+
+cv2.imshow('img', img)
+cv2.imshow('img_blur', img_blur)
+cv2.imshow('img_gaussian_blur', img_gaussian_blur)
+cv2.imshow('img_median_blur', img_median_blur)
+cv2.waitKey(0)
