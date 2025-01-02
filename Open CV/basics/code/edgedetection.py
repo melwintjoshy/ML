dfe0483e7f6585afffb8d5277b9b32d3a36c2213
mj465 <melwin465@gmail.com>
2025-01-02 18:25:21 +0530

    update code

--- Open CV/basics/code/edgedetection.py
+++ Open CV/basics/code/edgedetection.py
@@ -4,9 +4,10 @@ import numpy as np
 image_path = r"C:\Users\melwi\Desktop\ML\project1\Open CV\basics\rono.webp"
 
 img = cv2.imread(image_path)
+blurred = cv2.GaussianBlur(img, (3, 5), 5)
 
-
-img_edge = cv2.Canny(img, 45, 250)
+#canny edge detection algorithm
+img_edge = cv2.Canny(blurred, 30, 200)
 
 #dilation - makes edges thicker
 img_edge_d = cv2.dilate(img_edge, np.ones((3, 3), dtype=np.int8))
