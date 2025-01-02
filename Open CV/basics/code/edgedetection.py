2fa5db78cc8d6ed4582e8f061041615adf4c907a
mj465 <melwin465@gmail.com>
2025-01-02 18:19:01 +0530

    initial commit

---
+++ Open CV/basics/code/edgedetection.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+image_path = r"C:\Users\melwi\Desktop\ML\project1\Open CV\basics\rono.webp"
+
+img = cv2.imread(image_path)
+
+
+img_edge = cv2.Canny(img, 45, 250)
+
+#dilation - makes edges thicker
+img_edge_d = cv2.dilate(img_edge, np.ones((3, 3), dtype=np.int8))
+
+#erosion - makes edges thinner
+img_edge_e = cv2.erode(img_edge_d, np.ones((3, 3), dtype=np.int8))
+
+cv2.imshow('img', img)
+cv2.imshow('img_edge', img_edge)
+cv2.imshow('img_edge_d', img_edge_d)
+cv2.imshow('img_edge_e', img_edge_e)
+cv2.waitKey(0)
